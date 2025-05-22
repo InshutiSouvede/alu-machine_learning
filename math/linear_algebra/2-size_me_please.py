3dba358198f752ddcc5dca4adb3ff099dc445dcc
InshutiSouvede <isouvede2@gmail.com>
2025-05-22 18:14:50 +0200

    feat: implement core concepts of vectors, matricess and NumPy operations

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""Mode contains method determine the shape of a given matrix  """
+
+
+def matrix_shape(matrix):
+    """Function that return the shape of matrix, all dimensions"""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append((len(matrix)))
+        matrix = matrix[0]
+
+    return shape
---
+++ math/linear_algebra/README.md
@@ -0,0 +1,1 @@
+This is the second project in machine learning where I will show you what I know on Linear algebra through working on given tasks.
